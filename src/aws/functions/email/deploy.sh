42da4089ad897ac8c9e42ecbb2b1b5e39925bcb7
Oscar-gg <oscar.arreola.jr@gmail.com>
2024-08-05 23:28:24 -0600

    docs: deploy lambda and public folder in bucket

---
+++ src/aws/functions/email/deploy.sh
@@ -0,0 +1,14 @@
+# Runs commands to generate .zip file for AWS Lambda function
+# https://youtu.be/7-CvGFJNE_o?si=evSpTfI1AuBK2YM2&t=224
+# Make sure to run this script from src/aws/functions/email directory
+
+pip install -t dependencies -r requirements.txt
+cd dependencies
+zip ../aws_lambda_artifact.zip -r .
+cd ..
+zip aws_lambda_artifact.zip -u main.py
+zip aws_lambda_artifact.zip -u email_utils.py
+
+# Add credentials for automatic reload
+zip aws_lambda_artifact.zip -u credentials.json
+zip aws_lambda_artifact.zip -u token.json
