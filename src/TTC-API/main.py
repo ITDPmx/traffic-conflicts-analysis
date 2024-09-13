9aee884c37afc2adf71ac82b4a0ccd19f4ab7a5d
Ale-Coeto <alecoetos@gmail.com>
2024-09-13 23:18:46 +0000

    Test run through command

--- src/TTC-API/main.py
+++ src/TTC-API/main.py
@@ -32,5 +32,5 @@ def read_root():
     return {"Message": "TCC-API"}
 
 # Entry point to run the server
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
